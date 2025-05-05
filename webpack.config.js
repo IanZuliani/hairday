2c35349a40d58936df274359eb23e911d6ce1e3a
Rubens Zuliani <rubenszuliani@gmail.com>
2025-05-05 16:15:30 +0100

    Installing Webpack Combines all javascript files into one when we build. "npm run build"

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+//using javascript in the node environment, we need to do the import like this
+const path = require("path")
+
+module.exports = {
+    target: "web",
+    mode: "development",
+
+    //we use the path. solve why in different operating systems the / is done the other way around
+    entry: path.resolve(__dirname, "src", "main.js"),//get the input file
+    output: {
+       filename: "main.js",//name of the file that will be generated after the build
+       path: path.resolve(__dirname, "dist"),//folder that will be generated
+    }
+}
