f4775583dd8bdc5c942b11fc37eb90bbad4734c1
Rubens Zuliani <rubenszuliani@gmail.com>
2025-05-06 21:07:23 +0100

    Content loading event We will cover the process of capturing the selected date and defining the minimum date in the input, in addition to the need to capture times dynamically from the HTML.

---
+++ src/modules/load.js
@@ -0,0 +1,4 @@
+//Capture the events when the screen finishes loading the elements, then we display the times
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Dom esta pronnto")
+})
