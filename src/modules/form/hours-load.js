c4febbbebf0ffdba0bf6891f8416172acb798530
Rubens Zuliani <rubenszuliani@gmail.com>
2025-05-10 11:44:19 +0100

    Rendering the schedules

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,12 +1,15 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+//Get the ul hours so we can add the li
+const hours = document.getElementById("hours") 
+
 export function hoursLoad({ date }){
     const opening = openingHours.map((hour)=>{
-        //recuperando apenas a hora
+        //retrieving only the time removes the : and puts the first position in the scheduleHour
     const [scheduleHour] = hour.split(":")
 
-        console.log(scheduleHour)
+        //console.log(scheduleHour)
 
         //add the time to the date and check if it is in the past
         const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
@@ -18,5 +21,21 @@ export function hoursLoad({ date }){
        }
     })
 
-    console.log(opening)
+
+
+
+    //rendering the schedules
+    opening.forEach(({hour, available}) => {
+        //creates the LI element
+        const li = document.createElement("li")
+        //add the hour class
+        li.classList.add("hour")
+        //if the schedule is avaliable add the hour-available class if not add the hour-unavailable class
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        //add time in LI
+        li.textContent = hour
+        //add LI in ul hours
+        hours.append(li)
+    })
 }
