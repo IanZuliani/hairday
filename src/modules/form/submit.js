9c87cc73eead5c2101ef5527339b6e5181ddd29b
Rubens Zuliani <rubenszuliani@gmail.com>
2025-05-10 12:48:53 +0100

    Formatting a selected time to send to the API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 //Today date for input
@@ -16,5 +17,39 @@ form.onsubmit = (event) => {
     //prevents form submission
     event.preventDefault()
 
-    console.log("ENVIADO")
+    try {
+
+        //retrieving client name
+        const name = clientName.value.trim()
+
+        if(!name){
+            return alert("Insert Person Name")
+        }
+
+        //retrieves the selected schedule
+        const hourSelected = document.querySelector(".hour-selected")
+        
+        if(!hourSelected){
+            return alert("select the appointment time")
+        }
+        
+        //Recuperar somente a hora
+        const [hour] = hourSelected.innerText.split(":")
+
+        //inserir a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        //gerar um ID
+        const id = new Date().getTime()
+
+        console.log({
+            id,
+            name,
+            when,
+        })
+    } catch (error) {
+        alert("Nao foi possivel realizar o agendamento")
+        console.log(error)
+    }
+
 }
