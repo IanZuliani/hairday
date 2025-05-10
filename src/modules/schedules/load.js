6a6f55b1ff0c8e024199b172099c4d6e1328be63
Rubens Zuliani <rubenszuliani@gmail.com>
2025-05-10 14:10:17 +0100

    Searching for appointments in the API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,4 +1,4 @@
-
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 //seleciona o imput de data
@@ -6,12 +6,14 @@ const selectedDate = document.getElementById("date")
 
 //search the API for appointments to load on the right side of the screen 
 //Available times (Future time + unscheduled time)
-export function schedulesDay(){
+export async function schedulesDay(){
   //obyten a data do input
   const date = selectedDate.value
 
+  //search for appointments in the API
+  const dailySchedules = await scheduleFetchByDay({date})
+  console.log(dailySchedules)
 
   //renders available hours
-
   hoursLoad({date})
 }
